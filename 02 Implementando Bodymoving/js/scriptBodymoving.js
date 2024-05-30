2b44ea1cf396379ed8e221877c1345783a2a8ea8
everctba <everson777@gmail.com>
2024-05-29 22:39:50 -0300

    feito funcionar animação bodymoving

---
+++ 02 Implementando Bodymoving/js/scriptBodymoving.js
@@ -0,0 +1,23 @@
+// const botao = document.getElementsByClassName(".quadrado");
+const botao = document.querySelector('.quadrado');
+var anim = document.querySelector('.bm');
+
+var itemAnima = lottie.loadAnimation({
+   wrapper: anim,
+   animType: 'svg',
+   loop: false,
+   autoplay: false,
+   path:  './js/data.json'
+});
+console.log(botao);
+// anim.addEventListener('click', () => {
+// });
+botao.addEventListener('click', () => {
+   console.log(botao);
+   itemAnima.goToAndPlay(0,true);
+});
+
+
+
+// console.log(botao);
+// console.log(itemAnima);
